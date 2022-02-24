5dec82d0f7080b19df60afd30213d67deb5f6100
Hoga <howard.huang18@gmail.com>
2022-02-24 17:41:22 +0800

    1. Add 3 pages for TEACHER feature, "teacherProfiles.jsp", "teacherSkills.jsp", "teacherCourses.jsp" by Hoga. 2. Fix bugs in "UpdateController.java" 3. Revise layout and shortcuts in "index.jsp", "dashboard.jsp", "profile.jsp" 4. Removed not used draft pages, dashboard.html, login.html, register.html

--- src/main/java/web/member/controller/UpdateController.java
+++ src/main/java/web/member/controller/UpdateController.java
@@ -1,6 +1,7 @@
 package web.member.controller;
 
-import java.sql.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
@@ -29,61 +30,63 @@ public class UpdateController {
 			HttpSession httpSession) {
 
 //		Date birthSql = Date.valueOf(birth);
-		MemberBean bean = new MemberBean();
-		bean.setMemid(memID);
-		bean.setEmail(email);
-		bean.setPassword(password);
-		bean.setUsername(username);
-		bean.setPhonenum(phonenum);
-		bean.setBirth(birth);
-		bean.setPhoto(image);
-		
-		MemberBean updateResult = memberService.update(bean);
-		System.out.println(updateResult);
-
-		return "member/dashboard";
-//	//???框架下怎麼String轉date.sql???
-//		Date birthSql = Date.valueOf(birth);
-//	//驗證
-//		Map<String, String> errors = new HashMap<String, String>();
-//		model.addAttribute("errors", errors);
-//		if (email.length() == 0) {
-//			errors.put("email", "請輸入email");
-//		}
-//		if (password.length() == 0) {
-//			errors.put("password", "請輸入密碼");
-//		}
-//		if (username.length() == 0) {
-//			errors.put("username", "請輸入使用者名稱");
-//		}
-//		if (phonenum.length() == 0) {
-//			errors.put("phoenum", "請輸入電話");
-//		}
-//		if (birth.length() == 0) {
-//			errors.put("birth", "請輸入生日");
-//		}
-//		if (!errors.isEmpty()) {
-//			return "/views/member/dashboard.jsp";
-//		}
+//		MemberBean bean = new MemberBean();
+//		bean.setMemid(memID);
+//		bean.setEmail(email);
+//		bean.setPassword(password);
+//		bean.setUsername(username);
+//		bean.setPhonenum(phonenum);
+//		bean.setBirth(birth);
+//		bean.setPhoto(image);
 //		
-////2.呼叫model
-//	//值的"存改" (by某Service)
-//		MemberBean memberBean = (MemberBean)httpSession.getAttribute("member");
-//		//???框架下只能一個個set嗎????
-//		memberBean.setEmail(email);
-//		memberBean.setPassword(password);
-//		memberBean.setUsername(username);
-//		memberBean.setPhonenum(phonenum);
-//		memberBean.setBirth(birthSql);
-//		MemberBean updateResult = memberService.update(memberBean);
-//	
-//	//結果的"action" (by值結果)
-//		httpSession.setAttribute("member", updateResult);
-//		if(updateResult == null) {
-//			return "/views/member/dashboard.jsp";
-//		} else {
-//			return "redirect:" + "/views/member/dashboard.jsp";
-//		}
+//		MemberBean updateResult = memberService.update(bean);
+//		System.out.println(updateResult);
+//		return "dashboard.jsp";
+//	//框架下怎麼String轉date.sql???
+//		Date birthSql = Date.valueOf(birth);
+		
+	//驗證
+		Map<String, String> errors = new HashMap<String, String>();
+		model.addAttribute("errors", errors);
+		if (email.length() == 0) {
+			errors.put("email", "請輸入email");
+		}
+		if (password.length() == 0) {
+			errors.put("password", "請輸入密碼");
+		}
+		if (username.length() == 0) {
+			errors.put("username", "請輸入使用者名稱");
+		}
+		if (phonenum.length() == 0) {
+			errors.put("phoenum", "請輸入電話");
+		}
+		if (birth == null) {
+			errors.put("birth", "請輸入生日");
+		}
+		if (!errors.isEmpty()) {
+			return "/views/member/dashboard.jsp";
+		}
+		
+//2.呼叫model
+	//值的"存改" (by某Service)
+		MemberBean memberBean = (MemberBean)httpSession.getAttribute("member");
+		//???框架下只能一個個set嗎????
+		memberBean.setMemid(memID);
+		memberBean.setEmail(email);
+		memberBean.setPassword(password);
+		memberBean.setUsername(username);
+		memberBean.setPhonenum(phonenum);
+		memberBean.setBirth(birth);
+		memberBean.setPhoto(image);
+		MemberBean updateResult = memberService.update(memberBean);
+	
+	//結果的"action" (by值結果)
+		httpSession.setAttribute("member", updateResult);
+		if(updateResult == null) {
+			return "/views/member/dashboard.jsp";
+		} else {
+			return "redirect:" + "/views/member/dashboard.jsp";
+		}
 	}
 	
 }
