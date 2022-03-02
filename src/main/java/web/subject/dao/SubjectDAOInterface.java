dbebdea35e0efeab8cb02ccba0833738d38ecfb5
LAPTOP-6IR4E4AF\Tibame_T14 <avenger79513@gmail.com>
2022-03-02 21:32:03 +0800

    Revert "改subject 和 memberbean"
    This reverts commit 6fa378d57331ac8ef8ee59e917f92752c51507da.

--- src/main/java/web/subject/dao/SubjectDAOInterface.java
+++ src/main/java/web/subject/dao/SubjectDAOInterface.java
@@ -2,7 +2,6 @@ package web.subject.dao;
 
 import java.util.List;
 
-import web.member.entity.MemberBean;
 import web.subject.entity.SubjectBean;
 
 public interface SubjectDAOInterface {
@@ -17,6 +16,4 @@ public interface SubjectDAOInterface {
 
 	public abstract List<SubjectBean> select(); //查詢(多筆)
 	
-	
-	public abstract List<MemberBean> findTeacher(String subjectname);
 }
